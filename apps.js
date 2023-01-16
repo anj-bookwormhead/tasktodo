5bc72f01189924101adad19e3485e6a3f1bda923
Angelic Sanoy <31028354+anj-bookwormhead@users.noreply.github.com>
2023-01-15 21:26:34 -0800

    Task To-Do v1
    To do tasks involved:
    
    - Adding tasks
    - Remove task to-do
    - Hiding task to-do

---
+++ apps.js
@@ -0,0 +1,42 @@
+var hideToggle = document.querySelector ('.btn-toggle');
+var ContainerList = document.querySelector ('.list-container ul');
+const btnCreate = document.getElementById('btn-main');
+var removeBtn = document.querySelector('.btn-remove');
+
+
+hideToggle.addEventListener("click", () => {
+	if (hideToggle.textContent == 'Hide List') {
+		hideToggle.style.backgroundColor = "#00DA84";
+		hideToggle.textContent = hideToggle.TextContent = "Show"
+		ContainerList.style.display = 'none'
+
+	  } else if (hideToggle.textContent == 'Show') {
+		hideToggle.style.backgroundColor = "#FF4A9E"
+		ContainerList.removeAttribute('style');
+		hideToggle.textContent = hideToggle.TextContent = "Hide List"
+	
+	  
+	} else {
+		
+	  }
+
+
+	});
+
+// input task
+btnCreate.addEventListener('click', () => {
+	const input = document.querySelector('.input-main');
+	const list = document.querySelector('ul');
+	list.insertAdjacentHTML( 
+	  'beforebegin', 
+	  `<li>${input.value}</i>` 
+	);
+	input.value = '';
+  });
+
+
+removeBtn.addEventListener('click', ()=>{
+	const lis = document.querySelector ('li:last-child');
+	lis.remove();
+
+})
